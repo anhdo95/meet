3c962ed4993324a4f2d7acfdced88d8e1672cf1c
Anh Do <do.dinh.anh@linecorp.com>
2022-05-29 00:26:54 +0700

    chore: add dashboard

---
+++ meet/public/vue/components/dashboard/index.js
@@ -0,0 +1,9 @@
+Vue.component("dashboard", {
+  template: `
+    <div class="dashboard">
+      <h1 class="dashboard__logo">
+        M<span class="dashboard__highlight">ee</span>t
+      </h1>
+    </div>
+  `,
+});
