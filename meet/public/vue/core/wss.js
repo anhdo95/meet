bad5f2b01afceecf19c339491e2d9180242474fa
Anh Do <do.dinh.anh@linecorp.com>
2022-05-29 22:03:56 +0700

    feat: send pre offer and listen to logs from the server

--- meet/public/vue/core/wss.js
+++ meet/public/vue/core/wss.js
@@ -3,7 +3,20 @@ const wss = {
   init() {
     wss.socket = io.connect();
   },
+
   onConnect(listener) {
     return wss.socket.on("connect", listener);
   },
+
+  onLog(listener) {
+    return wss.socket.on("log", listener);
+  },
+
+  onPreOfferAnswer(listener) {
+    return wss.socket.on('pre-offer-answer', listener);
+  },
+
+  emitPreOffer(data) {
+    return wss.socket.emit('pre-offer', data)
+  }
 };
