d30a3b2d880cc61ea212289bb8c4308b1c68a699
Anh Do <do.dinh.anh@linecorp.com>
2022-05-29 10:30:59 +0700

    chore: embed css and js files into index.html

--- meet/public/vue/components/dashboard/index.js
+++ meet/public/vue/components/dashboard/index.js
@@ -4,6 +4,11 @@ Vue.component("dashboard", {
       <h1 class="dashboard__logo">
         M<span class="dashboard__highlight">ee</span>t
       </h1>
+
+      <div class="dashboard__your-code-container">
+        <input class="dashboard__your-code" />
+        <copy-icon class="dashboard__copy-icon" />
+      </div>
     </div>
   `,
 });
