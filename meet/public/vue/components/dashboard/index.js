28ceb326518ee2fd485dc5273e400eba50131470
Anh Do <do.dinh.anh@linecorp.com>
2022-05-29 11:06:59 +0700

    feat: add your personal code and allow inputting your friend code

--- meet/public/vue/components/dashboard/index.js
+++ meet/public/vue/components/dashboard/index.js
@@ -5,9 +5,24 @@ Vue.component("dashboard", {
         M<span class="dashboard__highlight">ee</span>t
       </h1>
 
-      <div class="dashboard__your-code-container">
-        <input class="dashboard__your-code" />
-        <copy-icon class="dashboard__copy-icon" />
+      <p class="dashboard__howto">Talk with other friend by passing your personal code to him/her</p>
+      <div class="dashboard__personal">
+        <p>Your personal code</p>
+        <div class="dashboard__your-code-container">
+          <span class="dashboard__your-code">3-k38wEOV1NVuWcSAAAF</span>
+          <copy-icon class="dashboard__copy-icon" />
+        </div>
+      </div>
+
+      <div class="dashboard__friend">
+        <p>Your friend code</p>
+        <input class="dashboard__friend-code" type="text" value="1-k38wEOV1NVuWcSAAA0" />
+        <div class="dashboard__buttons">
+          <button class="dashboard__video-call">
+            <video-call-icon class="dashboard__video-call-icon" />
+            Video Call
+          </button>
+        </div>
       </div>
     </div>
   `,
